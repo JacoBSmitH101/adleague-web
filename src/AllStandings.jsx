fd37a6aac84aeb0b88c670c93c351e2cb24cf78a
JacoBSmitH101 <jacobsmith2005@hotmail.com>
2025-06-29 23:25:46 +0200

    fix: update player name header to include average in standings

--- src/AllStandings.jsx
+++ src/AllStandings.jsx
@@ -72,7 +72,7 @@ const AllTablesPage = () => {
                                     <thead className="bg-gray-700">
                                         <tr>
                                             <th className="p-2 text-left">
-                                                Name
+                                                Name{" "}
                                                 <span className="text-gray-400">
                                                     (avg)
                                                 </span>
